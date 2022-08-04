01bd5b2bc55eaf2eb77edc203d1e907bc4d7f5c5
bugxd <l.feistritzer@gmx.at>
2022-08-04 15:37:55 +0200

    fix display grid update
    display cells in tbl, render tbl every intervall

--- src/pages/Core.tsx
+++ src/pages/Core.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Col, Row } from 'react-bootstrap';
-import Canvas from '../components/Canvas';
 import Cell from '../wfc/cell';
 import { CELL_SIZE } from '../wfc/tiles';
 import WFCCore from '../wfc/WFCCore';
@@ -9,57 +7,69 @@ const GRID_COUNT: number = 10;
 const width: number = CELL_SIZE * GRID_COUNT;
 const height: number = CELL_SIZE * GRID_COUNT;
 
-var grid: Cell[] = [];
+function CorePage() {
+  const [grid, setGrid] = useState<Cell[][]>([]);
 
-const drawGrid = (ctx: CanvasRenderingContext2D) => {
-  ctx.strokeStyle = 'black';
+  const mapGrid = (toMap: Cell[]): Cell[][] => {
+    let g: Cell [][] = [];
 
-  grid.forEach(cell => {
+    for (var i = 0; i < GRID_COUNT; i++){
+      const start = i * GRID_COUNT;
+      const end = start + GRID_COUNT;
+
+      const row = toMap.slice(start, end);
+
+      g.push(row);
+    }
+
+    return g;
+  }
+
+  const renderTile  = (cell: Cell) => {
     const tile = cell.tileSvg ??
     `<rect width="60" height="60" style="fill:rgb(255,255,255);" />
     <text x="30" y="30" class="small">${cell.possible.length}</text>`;
 
-    const bs = `<svg viewBox='0 0 ${CELL_SIZE} ${CELL_SIZE}' height='${CELL_SIZE}' width='${CELL_SIZE}' xmlns='http://www.w3.org/2000/svg'>${tile}</svg>`;
-    const image = new Image();
-    image.src = `data:image/svg+xml;base64,${window.btoa(bs)}`;
 
-    ctx.drawImage(image, cell.x, cell.y);
-  });
-}
+    const image = `<svg viewBox='0 0 ${CELL_SIZE} ${CELL_SIZE}' height='${CELL_SIZE}' width='${CELL_SIZE}' xmlns='http://www.w3.org/2000/svg'>
+        ${tile}
+    </svg>`
 
-function CorePage() {
-  const [update, setUpdate] = useState<number>(0);
+    return (
+      <img
+        width = { CELL_SIZE }
+        height = { CELL_SIZE }
+        src={ `data:image/svg+xml;utf8,${image}`} alt={`cell_${cell.id}` }/>
+    );
+  }
 
   useEffect(() => {
-    const runWFC = async () => {
-      const wfcCore = new WFCCore(width, height, GRID_COUNT, CELL_SIZE);
-      while(wfcCore.remainingUncollapsedCells >0) {
-        grid = wfcCore.nextStep();
-        setUpdate((prevValue) => prevValue + 1);
+    const wfcCore = new WFCCore(width, height, GRID_COUNT, CELL_SIZE);
+
+    const interval = setInterval(() => {
+      if(wfcCore.remainingUncollapsedCells >0) {
+        const g = wfcCore.nextStep();
+
+        setGrid([...mapGrid(g)]);
+      } else {
+        alert("Done!");
+        clearInterval(interval);
       }
+    }, 1000);
 
-      setUpdate((prevValue) => prevValue + 1);
-    }
-
-    runWFC();
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return (
-    <>
-      <Row>
-        <Col>Update: { update }</Col>
-      </Row>
-      <Row>
-        <Col>
-          <Canvas
-            draw= { drawGrid }
-            width={ width }
-            height={ height }
-            update={ update }
-          />
-        </Col>
-      </Row>
-    </>
+    <table>
+      <tbody>
+        {grid.map((row, i) => {
+          return (<tr key={`row_${i}`}>{row.map((cell,j ) => (<td key={`row_${i}_cell_${j}`}>{renderTile(cell)}</td>))}</tr>)
+        })}
+      </tbody>
+    </table>
   );
 }
 
