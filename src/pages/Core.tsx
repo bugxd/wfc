87f93eb17c67e38802c6eed7a452689944f8fb30
bugxd <l.feistritzer@gmx.at>
2022-10-31 23:24:59 +0100

    show grid as image
    convert table layout to svg image

--- src/pages/Core.tsx
+++ src/pages/Core.tsx
@@ -8,8 +8,8 @@ import { initAdjacents } from '../wfc_core/adjacents';
 
 function CorePage() {
   const { state } = useContext(TilesContext);
-  const width: number = useMemo(() => state.cellSize * state.rows, [state.cellSize, state.rows]);
-  const height: number = useMemo(() => state.cellSize * state.cols, [state.cellSize, state.cols]);
+  const width: number = useMemo(() => state.cellSize * state.cols, [state.cellSize, state.cols]);
+  const height: number = useMemo(() => state.cellSize * state.rows, [state.cellSize, state.rows]);
 
   const [grid, setGrid] = useState<TileCell[][]>([]);
 
@@ -28,6 +28,31 @@ function CorePage() {
         width = { state.cellSize }
         height = { state.cellSize }
         src={ `data:image/svg+xml;utf8,${image}`} alt={`cell_${cell.row}_${cell.col}` }/>
+    );
+  }
+
+  const renderSvgCell = (cell: TileCell) => {
+    const y = cell.row * state.cellSize;
+    const x = cell.col * state.cellSize;
+    const offset = Math.round(state.cellSize/2);
+
+    return `<g x='${x}' y='${y}' height='${state.cellSize}' width='${state.cellSize}' transform='translate(${x}, ${y}) rotate(${cell.rotateDegree},${offset},${offset})'>
+      ${cell.svg}
+    </g>`
+  }
+
+  const renderSvg = (grid: TileCell[][]) => {
+    const svg = `<svg viewBox='0 0 ${width} ${height}' height='${width}' width='${height}' xmlns='http://www.w3.org/2000/svg'>
+      ${grid.map(row => {
+        return row.map(cell => {return renderSvgCell(cell)})
+      })}
+    </svg>`
+
+    return (
+      <img
+        width = { width }
+        height = { height }
+        src={ `data:image/svg+xml;utf8,${svg}`} alt={`svg`}/>
     );
   }
 
@@ -68,13 +93,16 @@ function CorePage() {
   }, [state.rows, state.cols, state.tiles, state.frequencies, state.cellSize, height, width]);
 
   return (
-    <table>
-      <tbody>
-        {grid.map((row, i) => {
-          return (<tr key={`row_${i}`}>{row.map((cell,j) => (<td key={`row_${i}_cell_${j}`}>{renderTile(cell)}</td>))}</tr>)
-        })}
-      </tbody>
-    </table>
+    <>
+      {renderSvg(grid)}
+    </>
+    // <table>
+    //   <tbody>
+    //     {grid.map((row, i) => {
+    //       return (<tr key={`row_${i}`}>{row.map((cell,j) => (<td key={`row_${i}_cell_${j}`}>{renderTile(cell)}</td>))}</tr>)
+    //     })}
+    //   </tbody>
+    // </table>
   );
 }
 
